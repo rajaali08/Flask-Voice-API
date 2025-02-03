59ea9ce0ab8b217a47b99f69079b976bade9151c
rajaali08 <rajaalibackup08@gmail.com>
2025-02-03 21:19:37 +0500

    Updated Flask to run with Gunicorn

--- app.py
+++ app.py
@@ -82,4 +82,4 @@ def predict():
 # ✅ Ensure the app runs on a proper port
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Use PORT from environment or default to 5000
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
